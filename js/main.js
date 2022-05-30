d5f350fd94be7003d30282e7e2eaa42c467e3c0b
Vladislav Irhin <irhin2011@yandex.ru>
2022-05-31 02:41:20 +0300

    added cards animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-let heightCards = document.querySelectorAll('.memory-card');
+let cards = document.querySelectorAll('.memory-card');
+let counter = 0;
+
+for (let card of cards) {
+	card.addEventListener('click', function clickCard() {
+		if (counter == 2) {
+			waiteTime(2, function() {
+				card.removeEventListener('click', clickCard);
+				for (let card of cards) {
+					card.classList.remove('active');
+				}
+				card.addEventListener('click', clickCard);
+				counter = 0;
+			});
+		} else if (counter <= 2) {
+			card.classList.add('active');
+		++counter;
+		}
+
+
+		
+	});
+}
+
+
+function waiteTime(seconds, func) {
+	let acounter = seconds;
+	
+	let timerId = setInterval(function() {
+		acounter--;
+		if (acounter == 0) {
+			clearInterval(timerId);
+			func();
+		}
+	}, 1000)
+}
+
+
+ 
+
+
+
+
+
